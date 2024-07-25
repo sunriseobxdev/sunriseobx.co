eb8e9ac4280707728422e027f2178e76a5714842
Doc <zachwayland@sunriseobx.com>
2024-07-25 14:41:03 -0400

    change about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -19,7 +19,7 @@ const About = () => {
       <section className="gap about-first">
         <div className="container">
           <div className="row">
-            <h2>Construction industry representing the interests of small and medium-sized building companies</h2>
+            <h2> The Outer Banks Premier Exterior Construction Service</h2>
           </div>
         </div>
         <div className="container">
