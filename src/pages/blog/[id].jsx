6e106d8a915a4ec5b6d5b4cdd84213bb58c96d29
sunriseobxdev <inquiries@sunriseobx.co>
2024-07-26 19:16:07 -0500

    update

--- src/pages/blog/[id].jsx
+++ src/pages/blog/[id].jsx
@@ -3,8 +3,14 @@ import Layouts from "@layouts/Layouts";
 import { getAllPostsIds, getPostData, getRelatedPosts } from "@library/posts";
 import Date from '@library/date';
 import ImageView from "@components/ImageView";
+import { FacebookShareButton, FacebookIcon } from "next-share";
 
 import PageBanner from "@components/PageBanner";
+import globalThis from "the-global-object";
+
+const getHref = () => globalThis.location;
+
+const ShareButton = () => <FacebookShareButton url={getHref()} quote={"Share"} hashtag={"#sunriseobx"}><FacebookIcon size={32} round /></FacebookShareButton>
 
 const PostsDetail = ( props ) => {
   
@@ -12,7 +18,7 @@ const PostsDetail = ( props ) => {
 
   return (
     <Layouts>
-      <PageBanner pageTitle={"Blog Detail"} pageDesc={"our values and vaulted us to the top of our industry."} />
+      <PageBanner pageTitle={"Sunrise Construction Blog"} pageDesc={"building for the Outer Banks, related musings."} />
 
       {/* Blog Style Three Start */}
       <section className="gap blog-style-one blog-detail detail-page">
@@ -77,114 +83,15 @@ const PostsDetail = ( props ) => {
                       Share this:
                     </p>
                     <ul>
-                      <li><a href="#" onClick={(e) => {e.preventDefault();}}>Facebook</a></li>
-                      <li><a href="#" onClick={(e) => {e.preventDefault();}}>Twitter</a></li>
-                      <li><a href="#" onClick={(e) => {e.preventDefault();}}>LinkedIn</a></li>
+                      <li><ShareButton /></li>
                     </ul>
                 </div>
 
-                <div className="category shape comments">
-                  <h3>Comments</h3>
-                  <ul>
-                    <li>
-                      <div className="comment">
-                        <div className="c-img">
-                          <figure>
-                            <img src="/img/comment-img-1.jpg" alt="Comment Image One" />
-                          </figure>
-                        </div>
-                        <div className="c-data">
-                          <h4>Jonathom Doe</h4>
-                          <span>July 31, 2022</span>
-                          <p>Delivered ye sportsmen zealously arranging frankness estimable as. Nay any article enabled musical shyness yet sixteen.</p>
-                          <a className="c-r-btn" href="#.">Reply</a>
-                        </div>
-                      </div>
-                    </li>
-                    <li>
-                      <div className="comment reply">
-                        <div className="c-img">
-                          <figure>
-                            <img src="/img/comment-img-2.jpg" alt="Comment Image Two" />
-                          </figure>
-                        </div>
-                        <div className="c-data">
-                          <h4>Jonathom Doe</h4>
-                          <span>July 31, 2022</span>
-                          <p>Delivered ye sportsmen zealously arranging frankness estimable as. Nay any article enabled musical shyness yet sixteen.</p>
-                          <a className="c-r-btn" href="#.">Reply</a>
-                        </div>
-                      </div>
-                    </li>
-                  </ul>
-                </div>
-                <div className="category shape form">
-                  <h3>Leave a Comment</h3>
-                  <p>Your email address will not be published.</p>
-                  <form onSubmit={(e) => {e.preventDefault();}}>
-                    <textarea placeholder="Comment" />
-                    <div className="row">
-                      <div className="form-group col-md-6">
-                        <input type="text" name="text" placeholder="Complete Name" />
-                      </div>
-                      <div className="form-group col-md-6">
-                        <input type="email" name="email" placeholder="Email Address" />
-                      </div>
-                    </div>
-                    <div className="row align-items-center form-check">
-                      <div className="col-lg-12 d-flex-all justify-content-start">
-                        <input type="checkbox" className="form-check-inputt" id="exampleCheck24" />
-                        <label className="form-check-labell" htmlFor="exampleCheck24"> Save my name, email, and website in this browser for the next time I comment.</label>
-                      </div>
-                    </div>
-                    <button type="submit" className="theme-btn">Post Comment <i className="fa-solid fa-angles-right"></i></button>
-                  </form>
-                </div>
               </div>
             </div>
 
             <div className="col-lg-4">
               <aside className="sidebar">
-                <div className="box recent-posts">
-                  <h3>Recent Posts</h3>
-                  <ul>
-                    <li>
-                      <p>Consulted admitting is power acuteness.</p>
-                      <a href="/blog/">
-                        <i className="fa-solid fa-arrow-up-long"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <p>Consulted admitting is power acuteness.</p>
-                      <a href="blog-detail.html">
-                        <i className="fa-solid fa-arrow-up-long"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <p>Consulted admitting is power acuteness.</p>
-                      <a href="blog-detail.html">
-                        <i className="fa-solid fa-arrow-up-long"></i>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-                <div className="box recent-cmnts">
-                  <h3>Recent Comments</h3>
-                  <ul>
-                    <li>
-                      <h4>Thomas Walkar</h4>
-                      <p>Consulted admitting is power acuteness.</p>
-                    </li>
-                    <li>
-                      <h4>Thomas Walkar</h4>
-                      <p>Consulted admitting is power acuteness.</p>
-                    </li>
-                    <li>
-                      <h4>Thomas Walkar</h4>
-                      <p>Consulted admitting is power acuteness.</p>
-                    </li>
-                  </ul>
-                </div>
                 <div className="box categories">
                   <h3>Categories</h3>
                   <ul>
@@ -206,41 +113,6 @@ const PostsDetail = ( props ) => {
                     <li>
                       <a href="#" onClick={(e) => {e.preventDefault();}}>
                         <p>Expansion</p>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-                <div className="box categories">
-                  <h3>Archives</h3>
-                  <ul>
-                    <li>
-                      <a href="#" onClick={(e) => {e.preventDefault();}}>
-                        <p>July 2021</p>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-                <div className="box categories">
-                  <h3>Meta</h3>
-                  <ul>
-                    <li>
-                      <a href="#" onClick={(e) => {e.preventDefault();}}>
-                        <p>Log in</p>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" onClick={(e) => {e.preventDefault();}}>
-                        <p>Entries feed</p>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" onClick={(e) => {e.preventDefault();}}>
-                        <p>Comments feed</p>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#" onClick={(e) => {e.preventDefault();}}>
-                        <p>WordPress.org</p>
                       </a>
                     </li>
                   </ul>
