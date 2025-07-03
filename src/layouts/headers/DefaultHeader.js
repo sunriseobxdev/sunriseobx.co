1fd4a13b9aaa9283fffa01a486c23b3a1b1db036
sunriseobxdev <inquiries@sunriseobx.co>
2025-07-03 18:33:09 +0000

    update

--- src/layouts/headers/DefaultHeader.js
+++ src/layouts/headers/DefaultHeader.js
@@ -17,7 +17,7 @@ const DefaultHeader = ({ contactButton, cartButton }) => {
     navItems.push(newobj);
   });
 
-  const [themeUI, setThemeUIToggle] = useLocalStorage('theme_ui', '');
+  const [themeUI, setThemeUIToggle] = useLocalStorage('theme_ui', true);
   const [desktopMenu, desktopMenuToggle] = useState(false);
   const [mobileMenu, mobileMenuToggle] = useState(false);
   const [cartOpen, setCartOpen] = useState(false);
@@ -58,14 +58,12 @@ const DefaultHeader = ({ contactButton, cartButton }) => {
   }
 
   useEffect(() => {
+    // Always set dark mode as default
+    document.body.classList.add('light-d');
+    
     const lightmodeToggle = document.querySelector('#theme-icon');
-
-    if ( themeUI ) {
-      document.body.classList.add('light-d');
+    if (lightmodeToggle) {
       lightmodeToggle.src = '/images/moon.png';
-    } else {
-      document.body.classList.remove('light-d');
-      lightmodeToggle.src = '/images/sun.png';
     }
   }, []);
 
@@ -111,14 +109,6 @@ const DefaultHeader = ({ contactButton, cartButton }) => {
                       </ul>
                       
                       <div className="extras">
-                        <div className="theme-color">
-                          <img
-                            src={ themeUI ? "/images/moon.png" : "/images/sun.png"}
-                            alt="theme color"
-                            id="theme-icon"
-                            onClick={() => clickedThemeUI() }
-                          />
-                        </div>
                         <a href="#" id="mobile-menu" className={mobileMenu ? "menu-start open" : "menu-start"} onClick={ (e) => clickedMobileMenu(e) }>
                           <svg id="ham-menu" viewBox="0 0 100 100"> <path className="line line1" d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" /> <path className="line line2" d="M 20,50 H 80" /> <path className="line line3" d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942" /> </svg>
                         </a>
